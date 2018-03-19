f7b2e5564613fb78f85d842e4b6ddbe965a73ec7
Oleg Silvestrov <silvedko@gmail.com>
2018-03-19 18:20:46 +0200

    Add quiz

---
+++ Assets/Scripts/MVC/WindowController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainWindowController : MonoBehaviour
+{
+
+    public void SubscribeOnButtons ()
+    {
+
+    }
+
+    public void UnSubscribeFromButtons ()
+    {
+        Debug.Log("hi there, man!");
+    }
+
+    void Start ()
+    {
+        SubscribeOnButtons();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
